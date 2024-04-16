f29900311c6289342d15f35441fdf617d6a134ff
Daniel Poon <danielpoon@192-168-1-108.tpgi.com.au>
2024-04-16 18:22:21 +1000

    part 1 dev-skills

--- daniel-poon/wk09 - starts 15th Apr/1-mon/express-dev-skills/models/skill.js
+++ daniel-poon/wk09 - starts 15th Apr/1-mon/express-dev-skills/models/skill.js
@@ -1,11 +1,10 @@
 const skills = [
-{id: 1, name: "JavaScript"},
-{id: 2, name: "Mongo-db"},
-{id: 3, name: "Express"},
-{id: 4, name: "CSS"},
-{id: 5, name: "HTML"},
-{id: 6, name: "React"},
-{id: 7, name: "Flutter"},
+{id: 1, name: "JavaScript", description: "hi", imgUrl: "https://durableprogramming.com/wp-content/uploads/2023/04/JavaScript-logo.png"},
+{id: 2, name: "Express", description: "hi", imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxU_u44DpbT-IfWu2brkRSU0D1HIGYYUnFpQoI1gHOUnfPI7aQcBCpXth9_dqPVwv2L4Y&usqp=CAU"},
+{id: 3, name: "CSS", description: "hi", imgUrl: "https://cdn.freebiesupply.com/logos/large/2x/css3-logo-png-transparent.png"},
+{id: 4, name: "HTML", description: "hi", imgUrl: "https://s3.amazonaws.com/tf-nightingale/2022/12/html1.jpg"},
+{id: 5, name: "React", description: "hi", imgUrl: "https://cdn.freebiesupply.com/logos/large/2x/react-logo-png-transparent.png"},
+{id: 6, name: "Flutter", description: "hi", imgUrl: "https://assets-global.website-files.com/5ee12d8d7f840543bde883de/5ef3a1148ac97166a06253c1_flutter-logo-white-inset.svg"},
 ]
 
 module.exports = {
