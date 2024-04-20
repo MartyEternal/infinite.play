583e3d1a590cac812dafc45b9f204b86450e6717
Zala <zala.developer@gmail.com>
2024-04-20 13:29:58 +0930

    complete My Dev Skills Part 1 and Part 2

--- bhagirathsinh-zala/wk08 - starts 8th Apr/3-sat/express-dev-skills/controllers/skills.js
+++ bhagirathsinh-zala/wk08 - starts 8th Apr/3-sat/express-dev-skills/controllers/skills.js
@@ -2,19 +2,48 @@ const Skill = require("../models/Skill");
 
 function index(req, res) {
   res.render("skills/index", {
-    categories: Skill.getSkillsCategory(),
+    skills: Skill.getAllSkills(),
+    title: "All Skills",
   });
 }
 
 function show(req, res) {
-  const category = req.params.category;
   res.render("skills/show", {
-    category,
-    skills: Skill.getSkills(category),
+    skill: Skill.getSkillById(req.params.id),
+    title: "Skill Details",
   });
+}
+
+function newSkill(req, res) {
+  res.render("skills/new", { title: "Add New Skill" });
+}
+
+function create(req, res) {
+  Skill.addSkill(req.body);
+  res.redirect("/skills");
+}
+
+function deleteById(req, res) {
+  Skill.deleteSkillById(req.params.id);
+  res.redirect("/skills");
+}
+
+function edit(req, res) {
+  const skill = Skill.getSkillById(req.params.id);
+  res.render("skills/edit", { skill, title: "Edit Skill" });
+}
+
+function updateById(req, res) {
+  Skill.updateSkillById(req.params.id, req.body);
+  res.redirect("/skills");
 }
 
 module.exports = {
   index,
   show,
+  create,
+  edit,
+  new: newSkill,
+  delete: deleteById,
+  update: updateById,
 };
--- bhagirathsinh-zala/wk08 - starts 8th Apr/3-sat/express-dev-skills/models/Skill.js
+++ bhagirathsinh-zala/wk08 - starts 8th Apr/3-sat/express-dev-skills/models/Skill.js
@@ -1,32 +1,50 @@
-const skills = {
-  frontend: [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React.js",
-    "Redux Toolkit",
-    "Styled Component",
-    "Bootstrap",
-    "jQuery",
-    "Tailwind",
-  ],
-  backend: ["Python", "JavaScript (Node.js)", "Django", "Express.js"],
-  databases: ["PostgreSQL", "MongoDB"],
-  versionControl: ["Git"],
-  testing: ["Jest", "Cypress"],
-  deployment: ["AWS", "Heroku", "Render", "GitLab CI"],
-  soft: ["problemSolving", "communication", "teamwork", "timeManagement"],
-};
+const skills = [
+  { id: 1, name: "JavaScript", level: "Intermediate" },
+  { id: 2, name: "HTML", level: "Intermediate" },
+  { id: 3, name: "CSS", level: "Intermediate" },
+  { id: 4, name: "React", level: "Beginner" },
+  { id: 5, name: "Node.js", level: "Beginner" },
+];
 
-function getSkillsCategory() {
-  return Object.keys(skills);
+function getAllSkills() {
+  return skills;
 }
 
-function getSkills(category) {
-  return skills[category];
+function getSkillById(id) {
+  console.log(id);
+  id = parseInt(id);
+  return skills.find((skill) => skill.id === id);
+}
+
+function addSkill(skill) {
+  skill.id = Date.now() * 1000000;
+  console.log("Add Skill: ", skill);
+  skills.push(skill);
+}
+
+function deleteSkillById(id) {
+  id = parseInt(id);
+
+  const idx = skills.findIndex((skill) => skill.id === id);
+  skills.splice(idx, 1);
+}
+
+function updateSkillById(id, updatedSkill) {
+  id = parseInt(id);
+
+  const idx = skills.findIndex((skill) => skill.id === id);
+  // console.log("Updated Skill Details: ", updatedSkill);
+  if (idx !== -1) {
+    // console.log("Skill ID: ", skills[idx]);
+    skills[idx].name = updatedSkill.name;
+    skills[idx].level = updatedSkill.level;
+  }
 }
 
 module.exports = {
-  getSkillsCategory,
-  getSkills,
+  getAllSkills,
+  getSkillById,
+  addSkill,
+  deleteSkillById,
+  updateSkillById,
 };
