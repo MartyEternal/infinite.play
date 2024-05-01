f43ea138d4e9555f25bc5ad3bdf8aab764c05b7e
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2024-05-01 17:04:26 +1000

    finish minimum step12

--- mutsumi-ishihara/wk10 - starts 22nd Apr/3-sat/mongoose-flights/controllers/flights.js
+++ mutsumi-ishihara/wk10 - starts 22nd Apr/3-sat/mongoose-flights/controllers/flights.js
@@ -3,6 +3,7 @@ const Flight = require('../models/flight')
 module.exports = {
     all,
     new: newFlight,
+    addFlight
 }
 
 async function all(request, response) {
@@ -11,6 +12,22 @@ async function all(request, response) {
 }
 
 function newFlight(request, response) {
-    // should I add an object to pass data in ejs view?
     response.render('./flights/new')
+}
+
+async function addFlight(request, response) {
+    // should I add an object to pass data in ejs view?
+    if(request.body.flightNo.length < 2) {
+        response.render('flights/new')
+    }
+    // for(let key in req.body) {
+    //     if(request.body[key] === "") delete request.body[key]
+    // }
+    try {
+        await Flight.create(request.body)
+        response.redirect('flights')
+    } catch(err) {
+        console.log(err)
+        response.render('flights/new', { errorMsg: err.message})
+    }
 }
